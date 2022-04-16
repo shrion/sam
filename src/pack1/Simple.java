74617a24fa8ebfc3e4f0b57dda09b005ebcedafd
Admin <Admin@DESKTOP-F6OD9A9>
2022-04-16 16:12:34 +0530

    commuting

--- src/pack1/Simple.java
+++ src/pack1/Simple.java
@@ -2,9 +2,10 @@ package pack1;
 
 public class Simple {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("this is simple project");
+        System.out.println("this is simpe git project");
 	}
 
 }
